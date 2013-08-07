6f10f2ac888bd0fbc17494705f5c61a48e81849d
Joe Talerico <jtaleric@redhat.com>
2013-08-07 15:30:54 -0400

    Finishing up Cassandra Seed start (not tested). Added helper scripts to launch cassandra

--- scripts/cassandra_ycsb/cb_restart_seed.sh
+++ scripts/cassandra_ycsb/cb_restart_seed.sh
@@ -25,6 +25,7 @@ if [ $standalone == offline ] ; then
 fi
 
 SHORT_HOSTNAME=$(uname -n| cut -d "." -f 1)
+MY_IP=$(ifconfig eth0 | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1')
 
 cassandra=`get_ips_from_role cassandra`
 if [ -z $cassandra ] ; then
@@ -46,8 +47,13 @@ do
         ((pos++))
 done
 
+#
 # Update Cassandra Config
-
+#
+sudo sed -i 's/initial_token: $/initial_token: 0/g' /etc/cassandra/conf/cassandra.yaml
+sudo sed -i 's/- seeds: $/- seeds: $seed/g' /etc/cassandra/conf/cassandra.yaml
+sudo sed -i 's/listen_address: $/listen_address: $MY_IP/g' /etc/cassandra/conf/cassandra.yaml
+sudo sed -i 's/rpc_address: $/rpc_address: 0\.0\.0\.0/g' /etc/cassandra/conf/cassandra.yaml
 
 #
 # Remove possible old runs
@@ -55,7 +61,16 @@ done
 rm -rf /var/lib/cassandra/saved_caches/*
 rm -rf /var/lib/cassandra/data/system/*
 
+
+#
+# Start the database
+#
 syslog_netcat "Starting cassandra on ${SHORT_HOSTNAME}" 
 sudo service cassandra start 
+
+#
+# Init database
+#
+cassandra-cli -h $MY_IP -f cassandra-init.cassandra
 
 provision_application_stop $START
