173c9ff8f29c7f30412a4938cb7629fb4d7f52da
Joe Talerico <jtaleric@redhat.com>
2013-11-18 13:47:24 -0500

    airesize working, added Apache2

--- scripts/cassandra_ycsb/cb_modify_node.sh
+++ scripts/cassandra_ycsb/cb_modify_node.sh
@@ -3,13 +3,20 @@
 #/*******************************************************************************
 # This source code is provided as is, without any express or implied warranty.
 # 
-# cb_modify_node.sh -
-#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#     http://www.apache.org/licenses/LICENSE-2.0
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 #
 # @author: Joe Talerico - jtaleric@redhat.com
 #/*******************************************************************************
 
-cd ~
+cd /home/cbuser/ 
 source ~/.bashrc
 dir=$(echo $0 | sed -e "s/\(.*\/\)*.*/\1.\//g")
 if [ -e $dir/cb_common.sh ] ; then
