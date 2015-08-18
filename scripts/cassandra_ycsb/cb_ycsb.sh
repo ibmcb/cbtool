f954769cc799c2aabada90df03f0f4251f31e74f
Chris Schultz <cpschult@us.ibm.com>
2015-08-17 22:22:18 -0500

    Don't delete cassandra result file. This is useful for debugging and reviewing results.

--- scripts/cassandra_ycsb/cb_ycsb.sh
+++ scripts/cassandra_ycsb/cb_ycsb.sh
@@ -179,12 +179,11 @@ then
     syslog_netcat "Command line is: ${CMDLINE}. Output file is ${OUTPUT_FILE}"
     if [[ $APP_COLLECTION == "lazy" ]]
     then
-		lazy_collection "$CMDLINE" ${OUTPUT_FILE} ${SLA_RUNTIME_TARGETS}
+        lazy_collection "$CMDLINE" ${OUTPUT_FILE} ${SLA_RUNTIME_TARGETS}
     else
-    	eager_collection "$CMDLINE" ${OUTPUT_FILE} ${SLA_RUNTIME_TARGETS}
+        eager_collection "$CMDLINE" ${OUTPUT_FILE} ${SLA_RUNTIME_TARGETS}
     fi
     
-    rm ${OUTPUT_FILE}
 else
     syslog_netcat "This AI reached the limit of load generation process executions. If you want this AI to continue to execute the load generator, reset the \"run_limit\" counter"
     sleep ${LOAD_DURATION}
