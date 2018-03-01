65e0bd1c751f6e9bc0b46aa34986bf2ea541df66
Marcio Silva <marcios@us.ibm.com>
2018-03-01 10:57:30 -0500

    Image building and initial installation improvements
    1) A new script, "build_cloud_config_file_from_env.sh" can be
    used to programmatically build cloud configuration files on the fly.
    
    For instance, the following sequence of commands will create a new
    cloud configuration file connected to an OpenStack Cloud:
    export CB_OSK_ACCESS=AUTO; export CB_OSK_CREDENTIALS=~/openrc;
    export CB_OSK_SECURITY_GROUPS=default;
    export CB_OSK_INITIAL_VMCS=RegionOne:sut; export CB_OSK_LOGIN=fedora;
    export CB_OSK_NETNAME=flat_net; export CB_STARTUP_CLOUD=MYOSK;
    ./configs/build_cloud_config_file_from_env.sh
    
    2) A second script, "cbdocker" allows the execution of the CB
    Orchestrator directly on a Docker. For instance, the invocation
    `./cbdocker -d` will result in the execution of the CB Orchestrator
    CLI directly on a docker (omit the `-d` option for non interactive mode)
    All environment variables briefly mentioned above are passed directly
    to the docker, and thus the sequence "export CB_OSK_ACCESS=AUTO;
    export CB_OSK_CREDENTIALS=~/openrc;
    export CB_OSK_SECURITY_GROUPS=default;
    export CB_OSK_INITIAL_VMCS=RegionOne:sut; export CB_OSK_LOGIN=fedora;
    export CB_OSK_NETNAME=flat_net; export CB_STARTUP_CLOUD=MYOSK;
    ./cbdocker" would result in an Orchestrator, running entirely on
    a docker, connected to an OpenStack cloud.
    The default repository for the docker image is
    https://hub.docker.com/u/ibmcb/ubuntu_cb_orchestrator:experimental
    
    3) Additional fixes for docker image building.
    
    4) Additional fixes for automated installer.

--- docker/build_common.sh
+++ docker/build_common.sh
@@ -1,23 +1,58 @@
 #!/usr/bin/env bash
 
-function cb_docker_build {
-    CB_REPOSITORY=$1
-    CB_VERBQUIET=$2    
-    CB_DOCKERFN=$3
-    CB_USERNAME=$4 
-    CB_ARCH=$5
-    CB_RSYNC=$6
-    CB_FATAL=$7
-    CB_SQUASH=$8
+if [ $0 != "-bash" ] ; then
+    pushd `dirname "$0"` 2>&1 > /dev/null
+fi
+CB_DOCKER_BASE_DIR=$(pwd)
+if [ $0 != "-bash" ] ; then
+    popd 2>&1 > /dev/null
+fi
 
-    if [[ $CB_ARCH == "x86_64" ]]
+CB_REPO=cloudbench-docker-local.artifactory.swg-devops.com
+CB_WKS="ALL"
+CB_RSYNC_ADDR=$(sudo ifconfig docker0 | grep "inet " | awk '{ print $2 }' | sed 's/addr://g')
+for pi in $(sudo netstat -puntel | grep rsync | grep tcp[[:space:]] | awk '{ print $9 }' | sed 's^/rsync^^g')
+do
+    if [[ $(echo $(sudo ps aux | grep $pi | grep -c $(whoami)_rsync.conf)) -ne 0 ]]
+    then
+        CB_RSYNC_PORT=$(sudo netstat -puntel | grep $pi | awk '{ print $4 }' | cut -d ':' -f 2)
+	break
+    fi
+done
+CB_RSYNC=$CB_RSYNC_ADDR:${CB_RSYNC_PORT}/$(whoami)_cb
+CB_UBUNTU_BASE=ubuntu:16.04
+CB_PHUSION_BASE=phusion/baseimage:latest
+CB_CENTOS_BASE=centos:latest
+CB_VERB="-q"
+CB_PUSH="nopush"
+CB_ARCH=$(uname -a | awk '{ print $12 }')
+CB_PALL=0
+CB_USERNAME="cbuser"
+CB_MYUSERNAME=$(whoami)
+CB_MYUID=$(id -u $(whoami))
+CB_MYGID=$(id -g $(whoami))
+CB_BRANCH="master"
+CB_USAGE="Usage: $0 [-r <repository>] [-u Ubuntu base image] [-p Phusion base image] [-c Centos base image] [-w Workload] [-l CB Username/login] [-b branch] [--verbose] [--push] [--psall]"
+
+function cb_docker_build {
+    _CB_REPOSITORY=$1
+    _CB_VERBQUIET=$2    
+    _CB_DOCKERFN=$3
+    _CB_BRANCH=$4     
+    _CB_USERNAME=$5 
+    _CB_ARCH=$6
+    _CB_RSYNC=$7
+    _CB_FATAL=$8
+    _CB_SQUASH=$9
+
+    if [[ ${_CB_ARCH} == "x86_64" ]]
     then
         CB_ARCH1=x86_64
         CB_ARCH2=x86-64
         CB_ARCH3=amd64
     fi
 
-    if [[ $CB_ARCH == "ppc64le" ]]
+    if [[ ${_CB_ARCH} == "ppc64le" ]]
     then
         CB_ARCH1=ppc64le
         CB_ARCH2=ppc64
@@ -25,19 +60,21 @@ function cb_docker_build {
     fi                        
 
     CB_ACTUAL_SQUASH=''
-    if [[ ! -z $CB_SQUASH ]]
+    if [[ ! -z ${_CB_SQUASH} ]]
     then
-        if [[ $CB_SQUASH == "true" ]]
+        if [[ ${_CB_SQUASH} == "true" ]]
         then
-#	        CB_ACTUAL_SQUASH="--squash"
-	        CB_ACTUAL_SQUASH=""
+#            CB_ACTUAL_SQUASH="--squash"
+            CB_ACTUAL_SQUASH=""
         fi
     fi                                                                                                                                                                                                                        
-    sudo rm -rf Dockerfile && sudo cp -f $CB_DOCKERFN Dockerfile
+    sudo rm -rf Dockerfile && sudo cp -f ${_CB_DOCKERFN} Dockerfile
 
-    sudo sed -i "s^REPLACE_USERNAME^$CB_USERNAME^g" Dockerfile    
+    sudo sed -i "s^REPLACE_USERNAME^${_CB_USERNAME}^g" Dockerfile    
             
-    CB_DNAME=$(echo $CB_DOCKERFN | sed 's/Dockerfile-//g')
+    CB_DNAME=$(echo ${_CB_DOCKERFN} | sed 's/Dockerfile-//g')
+
+    sudo sed -i "s^REPLACE_BRANCH^${_CB_BRANCH}^g" Dockerfile
 
     sudo sed -i "s^REPLACE_ARCH1^$CB_ARCH1^g" Dockerfile
     sudo sed -i "s^REPLACE_ARCH2^$CB_ARCH2^g" Dockerfile
@@ -45,81 +82,149 @@ function cb_docker_build {
     sudo sed -i "s^REPLACE_BASE_VANILLA_UBUNTU^$CB_UBUNTU_BASE^g" Dockerfile
     sudo sed -i "s^REPLACE_BASE_VANILLA_PHUSION^$CB_PHUSION_BASE^g" Dockerfile
     sudo sed -i "s^REPLACE_BASE_VANILLA_CENTOS^$CB_CENTOS_BASE^g" Dockerfile
-    sudo sed -i "s^REPLACE_RSYNC^rsync -a rsync://$CB_RSYNC/3rd_party/workload/^g" Dockerfile
+    sudo sed -i "s^REPLACE_RSYNC^rsync -a rsync://${_CB_RSYNC}/3rd_party/workload/^g" Dockerfile
                 
     if [[ ! -z $CB_DNAME_BASE_UBUNTU ]]
     then
-        sudo sed -i "s^REPLACE_BASE_UBUNTU^${CB_REPOSITORY}/$CB_DNAME_BASE_UBUNTU^g" Dockerfile
+        sudo sed -i "s^REPLACE_BASE_UBUNTU^${_CB_REPOSITORY}/$CB_DNAME_BASE_UBUNTU^g" Dockerfile
     fi
 
     if [[ ! -z $CB_DNAME_BASE_PHUSION ]]
     then    
-        sudo sed -i "s^REPLACE_BASE_PHUSION^${CB_REPOSITORY}/$CB_DNAME_BASE_PHUSION^g" Dockerfile
+        sudo sed -i "s^REPLACE_BASE_PHUSION^${_CB_REPOSITORY}/$CB_DNAME_BASE_PHUSION^g" Dockerfile
     fi
     
     if [[ ! -z $CB_DNAME_BASE_CENTOS ]]
     then        
-        sudo sed -i "s^REPLACE_BASE_CENTOS^${CB_REPOSITORY}/$CB_DNAME_BASE_CENTOS^g" Dockerfile
+        sudo sed -i "s^REPLACE_BASE_CENTOS^${_CB_REPOSITORY}/$CB_DNAME_BASE_CENTOS^g" Dockerfile
     fi
 
     if [[ ! -z $CB_DNAME_NULLWORKLOAD_UBUNTU ]]
     then
-        sudo sed -i "s^REPLACE_NULLWORKLOAD_UBUNTU^${CB_REPOSITORY}/$CB_DNAME_NULLWORKLOAD_UBUNTU^g" Dockerfile
+        sudo sed -i "s^REPLACE_NULLWORKLOAD_UBUNTU^${_CB_REPOSITORY}/$CB_DNAME_NULLWORKLOAD_UBUNTU:${_CB_BRANCH}^g" Dockerfile
     fi
 
     if [[ ! -z $CB_DNAME_NULLWORKLOAD_PHUSION ]]
     then      
-        sudo sed -i "s^REPLACE_NULLWORKLOAD_PHUSION^${CB_REPOSITORY}/$CB_DNAME_NULLWORKLOAD_PHUSION^g" Dockerfile
+        sudo sed -i "s^REPLACE_NULLWORKLOAD_PHUSION^${_CB_REPOSITORY}/$CB_DNAME_NULLWORKLOAD_PHUSION:${_CB_BRANCH}^g" Dockerfile
     fi
 
     if [[ ! -z $CB_DNAME_NULLWORKLOAD_CENTOS ]]
     then          
-        sudo sed -i "s^REPLACE_NULLWORKLOAD_CENTOS^${CB_REPOSITORY}/$CB_DNAME_NULLWORKLOAD_CENTOS^g" Dockerfile
+        sudo sed -i "s^REPLACE_NULLWORKLOAD_CENTOS^${_CB_REPOSITORY}/$CB_DNAME_NULLWORKLOAD_CENTOS:${_CB_BRANCH}^g" Dockerfile
     fi
 
-    sudo cp -f Dockerfile ${CB_DOCKERFN}._processed_    
+    if [[ ! -z $CB_DNAME_PREREQS_UBUNTU ]]
+    then
+        sudo sed -i "s^REPLACE_PREREQS_UBUNTU^${_CB_REPOSITORY}/$CB_DNAME_PREREQS_UBUNTU^g" Dockerfile
+    fi
+    
+    if [[ ! -z $CB_DNAME_PREREQS_CENTOS ]]
+    then        
+        sudo sed -i "s^REPLACE_PREREQS_CENTOS^${_CB_REPOSITORY}/$CB_DNAME_PREREQS_CENTOS^g" Dockerfile
+    fi
+
+    sudo cp -f Dockerfile ${_CB_DOCKERFN}._processed_    
             
-    CB_ACTUAL_VERBQUIET=$(echo $CB_VERBQUIET | sed 's/--ve//g')
-    CB_DOCKER_CMD="sudo docker build -t ${CB_REPOSITORY}/$CB_DNAME $CB_ACTUAL_VERBQUIET $CB_ACTUAL_SQUASH ."
-    echo "########## Building image ${CB_REPOSITORY}/$CB_DNAME by executing the command \"$CB_DOCKER_CMD\" ..."
+    CB_ACTUAL_VERBQUIET=$(echo ${_CB_VERBQUIET} | sed 's/--ve//g')
+    CB_DOCKER_CMD="sudo docker build -t ${_CB_REPOSITORY}/$CB_DNAME:${_CB_BRANCH} $CB_ACTUAL_VERBQUIET $CB_ACTUAL_SQUASH ."
+    echo "########## Building image ${_CB_REPOSITORY}/$CB_DNAME by executing the command \"$CB_DOCKER_CMD\" ..."
     $CB_DOCKER_CMD
     ERROR=$?
     if [[ $ERROR -ne 0 ]]
     then
-        echo "############## ERROR: Image \"${CB_REPOSITORY}/$CB_DNAME\" failed while executing command \"$CB_DOCKER_CMD\""
+        echo "############## ERROR: Image \"${_CB_REPOSITORY}/$CB_DNAME\" failed while executing command \"$CB_DOCKER_CMD\""
         sudo rm -rf Dockerfile
-        if [[ $CB_FATAL == "true" ]]
+        if [[ ${_CB_FATAL} == "true" ]]
         then
             exit 1
         else
-            echo "############## WARNING: Image \"${CB_REPOSITORY}/$CB_DNAME\" will not be available for deployment!!!"
+            echo "############## WARNING: Image \"${_CB_REPOSITORY}/$CB_DNAME\" will not be available for deployment!!!"
             sleep 5
         fi
     else
-        echo "############## INFO: Image \"${CB_REPOSITORY}/$CB_DNAME\" built successfully!!!" 
+        echo "############## INFO: Image \"${_CB_REPOSITORY}/$CB_DNAME\" built successfully!!!" 
     fi
     sudo rm -rf Dockerfile
 }
 export -f cb_docker_build
+
+function cb_build_orchprereqs {
+    CB_REPOSITORY=$1
+    CB_VERBQUIET=$2
+    CB_USERNAME=$3
+    CB_ARCH=$4
+    CB_RSYNC=$5
+                            
+    echo "##### Building Docker orchestrator images"
+    pushd orchprereqs > /dev/null 2>&1
+    sudo rm -rf Dockerfile
+    for CB_DFILE in $(ls Dockerfile* | grep -v _processed_)
+    do
+        
+        CB_DNAME=$(echo $CB_DFILE | sed 's/Dockerfile-//g')
+        
+        echo $CB_DNAME | grep ubuntu > /dev/null 2>&1
+        if [[ $? -eq 0 ]]
+        then
+            export CB_DNAME_PREREQS_UBUNTU=$CB_DNAME
+        fi
+
+        echo $CB_DNAME | grep centos > /dev/null 2>&1
+        if [[ $? -eq 0 ]]
+        then
+            export CB_DNAME_PREREQS_CENTOS=$CB_DNAME
+        fi        
+        
+        cb_docker_build $CB_REPOSITORY $CB_VERBQUIET $CB_DFILE latest $CB_USERNAME $CB_ARCH $CB_RSYNC true
+    done
+    echo "##### Done building Docker orchestrator images"
+    echo
+    popd > /dev/null 2>&1    
+}
+export -f cb_build_orchprereqs
 
 function cb_build_orchestrator {
     CB_REPOSITORY=$1
     CB_VERBQUIET=$2
     CB_USERNAME=$3
     CB_ARCH=$4
-            
+    CB_RSYNC=$5
+    CB_BRANCH=$6
+
     echo "##### Building Docker orchestrator images"
     pushd orchestrator > /dev/null 2>&1
     sudo rm -rf Dockerfile
     for CB_DFILE in $(ls Dockerfile* | grep -v _processed_)
     do
-        cb_docker_build $CB_REPOSITORY $CB_VERBQUIET $CB_DFILE $CB_USERNAME $CB_ARCH NONE true
+        cb_docker_build $CB_REPOSITORY $CB_VERBQUIET $CB_DFILE $CB_BRANCH $CB_USERNAME $CB_ARCH $CB_RSYNC true
     done
     echo "##### Done building Docker orchestrator images"
     echo
     popd > /dev/null 2>&1    
 }
 export -f cb_build_orchestrator
+
+function cb_build_installtest {
+    CB_REPOSITORY=$1
+    CB_VERBQUIET=$2
+    CB_USERNAME=$3
+    CB_ARCH=$4
+    CB_RSYNC=$5
+    CB_BRANCH=$6
+                            
+    echo "##### Building Docker orchestrator images"
+    pushd installtest > /dev/null 2>&1
+    sudo rm -rf Dockerfile
+    for CB_DFILE in $(ls Dockerfile* | grep -v _processed_)
+    do
+        cb_docker_build $CB_REPOSITORY $CB_VERBQUIET $CB_DFILE $CB_BRANCH $CB_USERNAME $CB_ARCH $CB_RSYNC true
+    done
+    echo "##### Done building Docker orchestrator images"
+    echo
+    popd > /dev/null 2>&1    
+}
+export -f cb_build_installtest
 
 function cb_refresh_vanilla_images {
     CB_BUIM=$1
@@ -138,7 +243,7 @@ function cb_build_base_images {
     CB_USERNAME=$3
     CB_ARCH=$4
     CB_RSYNC=$5
-                  
+                      
     echo "##### Building Docker base images"
     pushd base > /dev/null 2>&1
     sudo rm -rf Dockerfile
@@ -164,7 +269,7 @@ function cb_build_base_images {
             export CB_DNAME_BASE_CENTOS=$CB_DNAME
         fi
 
-        cb_docker_build $CB_REPOSITORY $CB_VERBQUIET $CB_DFILE $CB_USERNAME $CB_ARCH $CB_RSYNC true false
+        cb_docker_build $CB_REPOSITORY $CB_VERBQUIET $CB_DFILE latest $CB_USERNAME $CB_ARCH $CB_RSYNC true false
     done
     echo "##### Done building Docker base images"
     echo
@@ -178,7 +283,8 @@ function cb_build_nullworkloads {
     CB_USERNAME=$3
     CB_ARCH=$4
     CB_RSYNC=$5
-            
+    CB_BRANCH=$6
+                
     echo "##### Building Docker nullworkload images"
     pushd workload > /dev/null 2>&1
     sudo rm -rf Dockerfile        
@@ -205,7 +311,7 @@ function cb_build_nullworkloads {
             export CB_DNAME_NULLWORKLOAD_CENTOS=$CB_DNAME
         fi                
                                                 
-        cb_docker_build $CB_REPOSITORY $CB_VERBQUIET $CB_DFILE $CB_USERNAME $CB_ARCH $CB_RSYNC true true
+        cb_docker_build $CB_REPOSITORY $CB_VERBQUIET $CB_DFILE $CB_BRANCH $CB_USERNAME $CB_ARCH $CB_RSYNC true true
     done
     echo "##### Done building Docker nullworkload images"
     echo
@@ -220,7 +326,8 @@ function cb_build_workloads {
     CB_ARCH=$4
     CB_WORKLOAD=$5
     CB_RSYNC=$6
-
+    CB_BRANCH=$7
+    
     if [[ $CB_WORKLOAD == "ALL" ]]
     then
         CB_WORKLOAD=''
@@ -231,7 +338,7 @@ function cb_build_workloads {
     echo "##### Building the rest of the Docker workload images"
     for CB_DFILE in $(ls Dockerfile*${CB_WORKLOAD} | grep -v nullworkload | grep -v _processed_ | grep -v ignore)
     do
-        cb_docker_build $CB_REPOSITORY $CB_VERBQUIET $CB_DFILE $CB_USERNAME $CB_ARCH $CB_RSYNC false false
+        cb_docker_build $CB_REPOSITORY $CB_VERBQUIET $CB_DFILE $CB_BRANCH $CB_USERNAME $CB_ARCH $CB_RSYNC false false
     done
     echo "##### Done building the rest of the Docker workload images"
     popd > /dev/null 2>&1
@@ -241,9 +348,33 @@ export -f cb_build_workloads
 function cb_push_images {
     CB_REPOSITORY=$1
     CB_PUSHALL=$2
+    CB_IMGTYPE=$3
+    CB_BRANCH=$4
     
+    if [[ -z $3 ]]
+    then
+        CB_IMGTYPE=$2
+    fi
+
+    if [[ -z $4 ]]
+    then
+        CB_BRANCH=""
+	else
+		CB_BRANCH=":"$CB_BRANCH 
+    fi
+            
+    if [[ $CB_IMGTYPE == "orchestrator" ]]
+    then
+        CB_IMG_GREP_CMD="grep $CB_IMGTYPE"
+    fi
+    
+    if [[ $CB_IMGTYPE == "workload" ]]
+    then
+        CB_IMG_GREP_CMD="grep -v orchestrator"
+    fi    
+
     echo "##### Pushing all images to Docker repository"
-    for IMG in $(docker images | grep ${CB_REPOSITORY} | awk '{ print $1 }')
+    for IMG in $(docker images | grep ${CB_REPOSITORY} | $CB_IMG_GREP_CMD | awk '{ print $1 }')
     do
         echo $IMG | grep coremark
         NOT_COREMARK=$?
@@ -261,10 +392,21 @@ function cb_push_images {
         NOT_RUBBOS=$?
         if [[ $NOT_COREMARK -eq 1 && $NOT_LINPACK -eq 1 && $NOT_PARBOIL -eq 1 && $NOT_SPEC -eq 1 && $NOT_CAFFE -eq 1 && $NOT_RUBIS -eq 1 && $NOT_RUBBOS -eq 1 || $CB_PUSHALL -eq 1 ]]
         then
-            CMD="docker push $IMG"
-            echo "########## Pushing image ${IMG} by executing the command \"$CMD\" ..."             
+            CMD="docker push ${IMG}${CB_BRANCH}"
+            echo "########## Pushing image ${IMG}${CB_BRANCH} by executing the command \"$CMD\" ..."             
             $CMD
         fi
     done
     echo "##### Images to Docker repository"    
+}
+
+function cb_remove_images {
+    CB_REPOSITORY=$1
+    CB_IMG_CLASS=$2
+    CB_BRANCH=$3
+    
+    for img in $(sudo docker images | grep $CB_REPO | grep $CB_IMG_CLASS | grep $CB_BRANCH | awk '{ print $3 }')
+    do
+        sudo docker rmi -f $img
+    done
 }
