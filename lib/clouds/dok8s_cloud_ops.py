6c5db6ff6a2851853390e03525611da02f4d21c1
Michael R. Hines <michael@hinespot.com>
2018-10-30 10:26:27 -0500

    Restore previous PRs
    Multiple bugfixes for the automated installer (Orchestrator),
    covering install (and reinstall) on both Ubuntu (Xenial) and CentOS.
    
    It is an attempt to solve issues reported during SPECCloud testing
    (TRAC #153)
    
    fix broken infinite retry
    
    This is a critical fix for public clouds: Infinite destroy was not
    working properly because the common logging code was throwing an
    exception on failure that we were not prepared to handle. Now we
    handle those and actually attempt to do the retry delete and it
    appears to be working now (primarily for GCE which throws a lot of
    timeout errors.)
    
    This commit does the following:
    
        We inherit from the base class in kub_cloud_ops.py and leave the file pretty much unchanged (some bugfixes, but no "core" changes to the base functionality, which we depend on)
        We then create DigitalOcean clusters on demand. (There is no libcloud support to do this, unfortunately. Yet.)
        Then we're able to use all of the existing k8s functions to create containers with very little changes.
        VMCs also can be fully attached in parallel: Both multiple clusters per region and across multiple regions.
        Next, we had to include "colocation" support into CB. This is because CB uses one container per pod rather than multiple application containers in the same pod. As a result, without a change in the CB "scheduler", so to speak, individual pods end up landing in the wrong clusters.
        Finally, it turns out that, due to the way k8s uses overlay networking between worker nodes, ganglia VM guest operating metrics don't use the original source IP addresses because even communication inside the cluster uses a NAT of some kind. This seems suboptimal, but fortunately, Ganglia has a workaround where you can ask Ganglia to override the hostname and IP that it uses with the one that you really want. That turned out to be a very easy change and should apply to all cloud providers and adapters anyway.
        vmcunregister also works as expected, as well as tool restart. Clusters get searched for and destroyed.
    
    Example configuration:
    
    [ USER-DEFINED : CLOUDOPTION_MYDOK8S ]
    DOK8S_INITIAL_VMCS = nyc1a,nyc1b # an arbitrary name. One must define these names below
    ==> Comma-separated groups of "name:region:k8s_version:k8s_droplet_size:k8s_workers_per_cluster"
    DOK8S_CLUSTERS = nyc1a:nyc1:1.11.1-do.1:4gb:7,nyc1b:nyc1:1.11.1-do.1:4gb:7
    DOK8S_CREDENTIALS = secret_token
    DOK8S_NETNAME = private # private == ClusterIP, public == LoadBalancer
    DOK8S_LOGIN = cbuser # The username that logins on the VMs
    
    [VM_DEFAULTS : DOK8S_CLOUDCONFIG]
    PORTS_BASE = 40000
    PORTS_RANGE = 5000 # Only necessary for DigitalOcean so far.
    
    [VMC_DEFAULTS : DOK8S_CLOUDCONFIG]
    PLACEMENT_METHOD = colocate,roundrobin # Very important
    
    It works!
    
    Because it's k8s, it has a really nice side effect that once the cluster is created, all future commands
    go to k8s, so in terms of error handling and things like that, all future code can remain
    in the original "kub" adapter without having to extend too much error handling to all of the
    other cloud providers.
    
    We should be able to use the file "dok8s_cloud_ops.py" as a pretty good template for the remaining clouds.

---
+++ lib/clouds/dok8s_cloud_ops.py
@@ -0,0 +1,290 @@
+#!/usr/bin/env python
+
+#/*******************************************************************************
+# Copyright (c) 2015 DigitalOcean, Inc.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+#/*******************************************************************************
+
+'''
+    Created on October 10, 2018
+    DigitalOcean Kubernetes Object Operations Library
+    @author: Michael R. Hines
+'''
+
+import requests
+import yaml
+from requests.adapters import HTTPAdapter
+from json import loads
+from time import sleep, time
+
+from lib.auxiliary.code_instrumentation import trace, cbdebug, cberr, cbwarn, cbinfo, cbcrit
+from shared_functions import CldOpsException
+
+from libcloud_common import LibcloudCmds
+from kub_cloud_ops import KubCmds
+
+import operator
+import traceback
+
+class Dok8sCmds(KubCmds) :
+    @trace
+    def __init__ (self, pid, osci, expid = None) :
+        KubCmds.__init__(self, pid, osci, expid = expid)
+
+    @trace
+    def get_description(self) :
+        return "DigitalOcean Kubernetes Cloud"
+
+    @trace
+    def connect(self, access, credentials, vmc_name, extra_parms = {}, diag = False, generate_rc = False) :
+        try :
+            extra_parms["kubeyaml"] = False
+            if not diag :
+                if "kubeconfig" in extra_parms :
+                    extra_parms["kubeyaml"] = yaml.safe_load(extra_parms["kubeconfig"])
+
+            return KubCmds.connect(self, access, credentials, vmc_name, extra_parms, diag, generate_rc)
+        except Exception, e :
+            for line in traceback.format_exc().splitlines() :
+                cbwarn(line, True)
+            raise e
+
+    @trace
+    def get_session(self) :
+        s = requests.Session()
+        s.mount('http', HTTPAdapter(max_retries=3))
+        s.mount('https', HTTPAdapter(max_retries=3))
+        s.headers.update(self.headers)
+
+        return s
+
+    @trace
+    def create_cluster(self, obj_attr_list) :
+        kuuid = False
+        kubeconfig = False
+
+        try :
+            s = self.get_session()
+
+            kname = "cb-" + obj_attr_list["username"] + '-' + obj_attr_list["cloud_name"].lower() + "-" + obj_attr_list["name"]
+
+            # First make sure there aren't any pre-existing clusters with this name.
+            # If so, destroy them.
+            r = s.get(self.access + "/kubernetes/clusters")
+
+            if r.status_code == 200 :
+                for cluster in r.json()["kubernetes_clusters"] :
+                    if cluster["name"] == kname :
+                        self.destroy_cluster(cluster["uuid"])
+            else :
+                cbdebug("Cluster cleanup failed: " + str(r.status_code), True)
+                raise CldOpsException("Could not cleanup old clusters.", 470)
+
+            cbdebug("Creating cluster for: " + obj_attr_list["name"], True)
+            create = {
+                "kubernetes_cluster": {
+                    "name": kname,
+                    "region": obj_attr_list["region"],
+                    "version" : obj_attr_list["k8s_version"],
+                    "worker_pools": [
+                        {
+                            "droplet_size" : obj_attr_list["k8s_worker_size"],
+                            "version" : obj_attr_list["k8s_version"],
+                            "num_nodes" : int(obj_attr_list["nb_workers"]),
+                            "name" : kname + "-pool"
+                        }
+                    ]
+                }
+            }
+
+            cbdebug("Requesting JSON: " + str(create))
+
+            # need tolerate errors, obviously
+            r = s.post(self.access + "/kubernetes/clusters", json = create)
+
+            if r.status_code == 201 :
+                j = r.json()
+                kuuid = j["kubernetes_cluster"]["uuid"]
+                cbdebug("Waiting for ready status uuid: " + kuuid)
+            else :
+                cbdebug("Create failed: " + str(r.status_code), True)
+                raise CldOpsException("No k8s for you. Code: " + str(r.status_code), 459)
+
+            while True :
+                r = s.get(self.access + "/kubernetes/clusters/" + kuuid)
+
+                if r.status_code == 200 :
+                    if not r.json()["kubernetes_cluster"]["pending"] :
+                        cbdebug("Done.")
+                        break
+
+                    sleep(5)
+                    continue
+
+            cbdebug("Getting kubeconfig for cluster " + kuuid)
+            r = s.get(self.access + "/kubernetes/clusters/" + kuuid + "/kubeconfig")
+
+            if r.status_code == 200 :
+                kubeconfig = r.text
+                cbdebug("Got kubeconfig")
+            else :
+                cbdebug("Failed to get kubeconfig: " + str(r.status_code))
+                raise CldOpsException("Failed to get kubeconfig: " + str(r.status_code), 460)
+
+            fwname = "k8s-" + kuuid + "-worker"
+            cbdebug("Modifying firewall " + fwname + " for this cluster...")
+
+            if r.status_code == 200 :
+                r = s.get(self.access + "/firewalls")
+            else :
+                cbdebug("Failed to get firewall " + fwname + ": " + str(r.status_code), True)
+                raise CldOpsException("No k8s for you.", 461)
+
+            firewalls = r.json()
+
+            fwuuid = False
+
+            for fw in firewalls["firewalls"] :
+                if fw['name'] == fwname :
+                    fwuuid = fw['id']
+                    cbdebug("Firewall found: " + fwuuid)
+
+            cbdebug("Adding rule to firewall " + fwuuid)
+
+            vm_defaults = self.osci.get_object(obj_attr_list["cloud_name"], "GLOBAL", False, "vm_defaults", False)
+            ports_base = int(vm_defaults["ports_base"])
+            ports_range = int(vm_defaults["ports_range"])
+            ports_end = ports_base + ports_range
+
+            rule = {
+              "inbound_rules": [
+                {
+                  "protocol": "tcp",
+                  "ports": str(ports_base) + "-" + str(ports_end),
+                  "sources": {
+                        "addresses": [
+                          "0.0.0.0/0",
+                          "::/0"
+                        ]
+                  }
+                }
+              ]
+            }
+
+            r = s.post(self.access + "/firewalls/" + fwuuid + "/rules", json = rule)
+
+            if r.status_code == 204 :
+                cbdebug("Successfully added firewall rule to " + fwuuid)
+            else :
+                cbdebug("Firewall rule add failed: " + str(r.status_code), True)
+        except Exception, e :
+            for line in traceback.format_exc().splitlines() :
+                cbwarn(line, True)
+
+            cberr("Failure to create k8s cluster: " + str(e), True)
+
+            if kuuid :
+                self.destroy_cluster(kuuid)
+
+            return False, False
+
+        return kuuid, kubeconfig
+
+    @trace
+    def destroy_cluster(self, kuuid) :
+        try :
+            s = self.get_session()
+
+            cbdebug("Destroying Cluster: " + kuuid, True)
+
+            r = s.delete(self.access + "/kubernetes/clusters/" + kuuid)
+
+            if r.status_code != 202 :
+                cbdebug("Failed to delete: " + str(r.status_code), True)
+                raise CldOpsException("Destroy cluster failed.", 462)
+                
+            # Check for delete complete....
+
+            cbdebug("Waiting for delete to finish...")
+            while True :
+                r = s.get(self.access + "/kubernetes/clusters/" + kuuid)
+
+                if r.status_code == 404 or (r.status_code == 200 and not r.json()["kubernetes_cluster"]["pending"]) :
+                    cbdebug("Done.")
+                    break
+
+                sleep(5)
+                continue
+                    
+            cbdebug("Deleted " + kuuid)
+            return True
+
+        except Exception, e :
+            for line in traceback.format_exc().splitlines() :
+                cbwarn(line, True)
+            cberr("Failure to destroy k8s cluster: " + str(e), True)
+            return False
+
+    @trace
+    def vmcregister(self, obj_attr_list) :
+        status = 0
+        msg = ""
+
+        try :
+            cluster_list = obj_attr_list["clusters"].lower().strip().split(",")
+            region = False
+            size = False
+            version = False
+            worker_size = False
+            for cluster in cluster_list :
+                name, region, version, worker_size, nb_workers = cluster.split(":")
+                if name == obj_attr_list["name"] :
+                    cbdebug("VMC " + name + " in " + region + " using version " + version + " and " + nb_workers + " workers each of size " + worker_size, True)
+                    break
+
+            if not region :
+                return 104, "VMC " + name + " not found in CLUSTERS configuration list. Please correct and try again: " + cluster_list
+
+            obj_attr_list["region"] = region
+            obj_attr_list["nb_workers"] = nb_workers
+            obj_attr_list["k8s_version"] = version
+            obj_attr_list["k8s_worker_size"] = worker_size
+
+            if "kubeconfig" not in obj_attr_list :
+                self.access = obj_attr_list["access"]
+                self.headers = {"Authorization" : "Bearer " + obj_attr_list["credentials"]}
+                obj_attr_list["kuuid"], obj_attr_list["kubeconfig"] = self.create_cluster(obj_attr_list)
+                if not obj_attr_list["kuuid"] :
+                    return 458, "vmcregister did not find a UUID, No k8s for you."
+            status, msg = KubCmds.vmcregister(self, obj_attr_list)
+        except Exception, e :
+            for line in traceback.format_exc().splitlines() :
+                cbwarn(line, True)
+            status, msg = self.common_messages("VMC", obj_attr_list, "registered", status, msg)
+
+        return status, msg
+
+    @trace
+    def vmcunregister(self, obj_attr_list) :
+        status = 0
+        msg = "" 
+        try :
+            self.access = obj_attr_list["access"]
+            self.headers = {"Authorization" : "Bearer " + obj_attr_list["credentials"]}
+            status, msg = KubCmds.vmcunregister(self, obj_attr_list)
+            if status == 0 and "kubeconfig" in obj_attr_list :
+                success = self.destroy_cluster(obj_attr_list["kuuid"])
+                if not success :
+                    status = 463
+                    msg = "Failed to destroy k8s cluster"
+        except Exception, e :
+            for line in traceback.format_exc().splitlines() :
+                cbwarn(line, True)
+            status, msg = self.common_messages("VMC", obj_attr_list, "unregistered", status, msg)
+        return status, msg 
+
