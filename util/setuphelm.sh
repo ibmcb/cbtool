4bbbb8fd68fc051aafdc2751e42f47bc0506135d
Michael R. Hines <michael@hinespot.com>
2019-02-21 20:58:13 -0600

    Need at least openvpn 2.4 for this to work.

--- util/setuphelm.sh
+++ util/setuphelm.sh
@@ -331,6 +331,8 @@ check_error $? "extract client certificate"
 
 echo "management 127.0.0.1 ${VPN_STATUS_PORT}" >> /tmp/${KEY_NAME}.ovpn
 echo "management-log-cache 10000" >> /tmp/${KEY_NAME}.ovpn
+# The helm chart unconditionally creates this route, which is wrong. Get rid of it.
+echo 'pull-filter ignore "route 10.0.0.0"' >> /tmp/${KEY_NAME}.ovpn
 
 echo "Starting VPN ..."
 sudo openvpn --config /tmp/${KEY_NAME}.ovpn --daemon
@@ -510,9 +512,6 @@ for node in $(kubectl get nodes | grep Ready | cut -d " " -f 1) ; do
 done
 
 kubectl delete --wait deployment backdoor
-
-# The helm chart unconditionally creates this route, which is wrong. Get rid of it.
-sudo ip route delete 10.0.0.0/8
 
 rm /tmp/backdoor.yaml
 rm ${dir}/portforward.sh 
