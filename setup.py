b01a656abfd7445c168e083a1aedd502bf108d02
Michael R. Galaxy <michael@hinespot.com>
2019-09-19 15:39:59 -0500

    PyPi additional tricks

--- setup.py
+++ setup.py
@@ -6,23 +6,32 @@ with open("README.md", "r") as fh:
 setuptools.setup(
   name = 'cbtool',
 
+
+  version = '2.0.11',
+  author = 'Marcio Silva, Michael Galaxy',
+  author_email = 'cbtool-admin@googlegroups.com',
+  description = 'CloudBench: Cloud Rapid Experimentation and Analysis Toolkit',
+  long_description=long_description,
+  long_description_content_type="text/markdown",
+  url = 'https://github.com/ibmcb/cbtool',
+
   # Currently, the only thing we provided on PyPi is the core library.
   # Getting a fully-fledged instance of CB installed will require
   # a proper DEB to be prepared. This just solves the issue of talking
   # talking to the cloudbench API using an already-installed version
   # of cloudbench. 
 
-  version = '2.0.6',
-  license='Apache v2',
-  description = 'CloudBench: Cloud Rapid Experimentation and Analysis Toolkit',
-  long_description=long_description,
-  long_description_content_type="text/markdown",
-  author = 'Marcio Silva, Michael Galaxy',
-  author_email = 'cbtool-admin@googlegroups.com',
-  url = 'https://github.com/ibmcb/cbtool',
+  packages = ['cbtool', 'cbtool/lib', 'cbtool/lib/api', 'cbtool/lib/auxiliary', 'cbtool/lib/clouds', 'cbtool/lib/operations', 'cbtool/lib/remote', 'cbtool/lib/stores'],
+
+  classifiers=[
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: Apache Software License',
+    'Programming Language :: Python :: 2.7',
+  ],
   download_url = 'https://github.com/ibmcb/cbtool/archive/2.0.tar.gz',
   keywords = ['cloudbench', 'cloud', 'benchmarking', "spec"],
-  packages = setuptools.find_packages(),
 
   # Currently, we've only listed packages listed as `= pip`
   # from the PUBLIC_dependencies.txt file, because pip install
@@ -58,11 +67,4 @@ setuptools.setup(
           'pillow',
           'jsonschema'
       ],
-  classifiers=[
-    'Development Status :: 5 - Production/Stable',
-    'Intended Audience :: Developers',
-    'Topic :: Software Development :: Build Tools',
-    'License :: OSI Approved :: Apache Software License',
-    'Programming Language :: Python :: 2.7',
-  ],
 )
